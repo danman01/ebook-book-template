637ec75b2a2e75acd7ab0eaf774a50da4fe42985
Jim Scott <jim@13ways.com>
2015-09-22 09:53:09 -0500

    adding scala syntax highlighting

--- includes/_thebe.js
+++ includes/_thebe.js
@@ -3,7 +3,7 @@
 		new Thebe({
 			url: "https://tmp35.tmpnb.org",
 			kernel_name: "scala",
-			codemirror_mode_name: "clike",
+			//codemirror_mode_name: "clike",
 			load_css: false,
 			inject_css: false,
 			add_interrupt_button: true
