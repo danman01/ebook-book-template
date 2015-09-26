ba563cdbe8facbd725f1d8f85585797ad7d38a49
Jim Scott <jim@13ways.com>
2015-09-26 09:26:04 -0500

    changed server and scala

--- includes/_thebe.js
+++ includes/_thebe.js
@@ -1,9 +1,9 @@
 <script>
 	$(function() {
 		new Thebe({
-			url: "https://tmp35.tmpnb.org",
+			url: "https://tmp39.tmpnb.org",
 			kernel_name: "scala",
-			codemirror_mode_name: "clike",
+			codemirror_mode_name: "text/x-scala",
 			load_css: false,
 			inject_css: false,
 			add_interrupt_button: true
